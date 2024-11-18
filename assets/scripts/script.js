04d866b3e2cbf2eab5b1eb5b5ee36d535010b3af
Muza21 <lasha.muzashvili1@gmail.com>
2024-11-19 02:09:53 +0400

    feat: add fetch data feature and render

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,57 +1,76 @@
-// 1.
-// const obj = {
-//   xy: 30,
-// };
-// obj.reverse()
-// obj.yx = 40;
-// obj.xy = 50;
+const productsList = [];
 
-// obj.xy10y = 20;
-// obj.yx10x = 20;
-
-// console.log(obj.xy);
-// OUTPUT: object reverse is not a function.
-
-// 2.
-// var x = 10;
-
-// function foo(foobar) {
-//   var x = 40;
-//   foobar();
-// }
-
-// function bar() {
-//   console.log(x);
-// }
-
-// foo(bar);
-// OUTPUT: 10
-
-// 3.
-// const something = new Set(["goal", "success", "failed"]);
-
-// const people = [
-//   { name: "nam1", age: 20, goal: "success" },
-//   { name: "nam2", age: 22, goal: "fail" },
-//   { name: "nam3", age: 23, goal: "goal" },
-// ];
-// const result = people
-//   .filter((person) => something.has(person.goal))
-//   .map((person) => person.name);
-
-// console.log(result);
-// OUTPUT: names of the people who have
-
-// 4.
-var obj = {
-  a: 2,
+const fetchData = async function () {
+  try {
+    const response = await fetch(
+      "https://dummyjson.com/products/category/smartphones"
+    );
+    const data = await response.json();
+    productsList.push(...data.products);
+    drawData(productsList);
+  } catch (e) {
+    console.error(`Error: ${e}`);
+  }
 };
 
-function clear() {
-  obj.a = 3;
-  obj = null;
+fetchData();
+
+const productsListContainer =
+  document.getElementsByClassName("products_list")[0];
+
+function drawData(data) {
+  const yellowStar = `
+  <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M8 12.0553L12.944 15L11.632 9.45L16 5.71579L10.248 5.23421L8 0L5.752 5.23421L0 5.71579L4.368 9.45L3.056 15L8 12.0553Z" fill="#FF9017"/>
+  </svg>
+  `;
+  const greyStar = `
+  <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M8 12.0553L12.944 15L11.632 9.45L16 5.71579L10.248 5.23421L8 0L5.752 5.23421L0 5.71579L4.368 9.45L3.056 15L8 12.0553Z" fill="#D5CDC5"/>
+  </svg>
+  `;
+  productsListContainer.innerHTML = "";
+  data.forEach((product) => {
+    const fullStarsNum = Math.round(product.rating / 2);
+    let starsRating = "";
+    for (let i = 0; i < fullStarsNum; i++) {
+      starsRating += yellowStar;
+    }
+    for (let i = fullStarsNum; i < 5; i++) {
+      starsRating += greyStar;
+    }
+    let discountedPrice = (
+      product.price -
+      (product.price * product.discountPercentage) / 100
+    ).toFixed(2);
+    const productItem = document.createElement("div");
+    productItem.className = "product_card";
+    productItem.innerHTML = `
+        <div class="image_container">
+          <img src="${product.thumbnail}" alt="product image" />
+        </div>
+        <div class="product_details">
+          <div>
+            <div class="price">
+              <span class="current_price">${discountedPrice}</span
+              ><span class="old_price">${product.price}</span>
+            </div>
+            <div class="rate">
+              ${starsRating}
+              <span class="rate_score">${product.rating}</span>
+            </div>
+          </div>
+          <div class="favourite">
+            <button>
+              <img
+                src="./assets/icons/favorite_border.svg"
+                alt="heart logo add to favourite"
+              />
+            </button>
+          </div>
+        </div>
+        <div class="product_name">${product.title}</div>
+    `;
+    productsListContainer.appendChild(productItem);
+  });
 }
-
-clear();
-
-console.log(obj);
