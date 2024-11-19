6cdbb6787d83e4b5fb4418fd0833a8e82d266fa5
Muza21 <lasha.muzashvili1@gmail.com>
2024-11-19 10:30:37 +0400

    feat: add pagination

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,6 +2,10 @@ const STARS_NUM = 5;
 const productsList = [];
 const productsListContainer =
   document.getElementsByClassName("products_list")[0];
+const paginationNav = document.querySelector(".pagination_nav");
+const pageSizeDisplay = document.querySelector("#display");
+let pageSize = pageSizeDisplay.value;
+let currentPage = 1;
 
 const fetchData = async function () {
   try {
@@ -17,6 +21,11 @@ const fetchData = async function () {
 };
 
 fetchData();
+
+pageSizeDisplay.addEventListener("change", (e) => {
+  pageSize = e.target.value;
+  drawData(productsList);
+});
 
 function drawStars(rating) {
   const yellowStar = `
@@ -44,29 +53,21 @@ function calcDiscountPrice(price, discount) {
   return (price - (price * discount) / 100).toFixed(2);
 }
 
-function drawData(data) {
-  productsListContainer.innerHTML = "";
-  data.forEach((product) => {
-    const starsRating = drawStars(product.rating);
-    const discountedPrice = calcDiscountPrice(
-      product.price,
-      product.discountPercentage
-    );
-    console.log(discountedPrice);
-    const productItem = document.createElement("div");
-    productItem.className = "product_card";
-    productItem.innerHTML = `
+function drawProductItem(product, price, stars) {
+  const productItem = document.createElement("div");
+  productItem.className = "product_card";
+  productItem.innerHTML = `
         <div class="image_container">
           <img src="${product.thumbnail}" alt="product image" />
         </div>
         <div class="product_details">
           <div>
             <div class="price">
-              <span class="current_price">${discountedPrice}</span
+              <span class="current_price">${price}</span
               ><span class="old_price">${product.price}</span>
             </div>
             <div class="rate">
-              ${starsRating}
+              ${stars}
               <span class="rate_score">${product.rating}</span>
             </div>
           </div>
@@ -81,6 +82,45 @@ function drawData(data) {
         </div>
         <div class="product_name">${product.title}</div>
     `;
+  return productItem;
+}
+
+function drawData(products) {
+  productsListContainer.innerHTML = "";
+  for (let i = (currentPage - 1) * pageSize; i < pageSize * currentPage; i++) {
+    if (!products[i]) {
+      break;
+    }
+    const product = products[i];
+    const starsRating = drawStars(product.rating);
+    const discountedPrice = calcDiscountPrice(
+      product.price,
+      product.discountPercentage
+    );
+    const productItem = drawProductItem(product, discountedPrice, starsRating);
     productsListContainer.appendChild(productItem);
-  });
+  }
+  drawPagination(products);
+}
+
+function onPaginationClick(nextPage) {
+  currentPage = nextPage;
+  drawData(productsList);
+}
+
+function drawPagination(products) {
+  paginationNav.innerHTML = "";
+  const pageCount = Math.ceil(products.length / pageSize);
+  for (let i = 0; i < pageCount; i++) {
+    const button = document.createElement("button");
+    button.innerText = i + 1;
+    if (currentPage === i + 1) {
+      button.classList.add("active");
+    }
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      onPaginationClick(i + 1);
+    });
+    paginationNav.appendChild(button);
+  }
 }
